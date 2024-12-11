e0a05431b63aa934ccbf00f8151baa7f4bd5e5d2
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2024-12-11 13:39:46 +1100

    Update query1.sql

--- ClickHouse/stream processing/part 1/query1.sql
+++ ClickHouse/stream processing/part 1/query1.sql
@@ -1,1 +1,13 @@
+SELECT 
+    t.timestamp AS toyTimestamp, 
+    t.toyType, 
+    t.toyId, 
+    b.timestamp AS boxTimestamp, 
+    b.boxId
+FROM 
+    toys t
+JOIN 
+    boxes b
+ON 
+    t.toyType = b.toyType
 
