5e1e0024c3d2883b0cae53b6802656c54148fcea
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2024-12-11 13:37:10 +1100

    Update createTables.sql

--- ClickHouse/stream processing/part 1/createTables.sql
+++ ClickHouse/stream processing/part 1/createTables.sql
@@ -1,1 +1,4 @@
+CREATE TABLE toys ( `timestamp` DateTime, `toyType` UInt32, `toyId` UInt32 ) ENGINE = MergeTree ORDER BY timestamp
+
+CREATE TABLE boxes ( `timestamp` DateTime, `toyType` UInt32, `boxId` UInt32 ) ENGINE = MergeTree ORDER BY timestamp
 
