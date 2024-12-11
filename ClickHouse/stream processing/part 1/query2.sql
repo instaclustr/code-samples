33c6abba4675387e881755170b01d618405c4132
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2024-12-11 13:41:11 +1100

    Update query2.sql

--- ClickHouse/stream processing/part 1/query2.sql
+++ ClickHouse/stream processing/part 1/query2.sql
@@ -1,1 +1,15 @@
+SELECT 
+    t.timestamp AS toyTimestamp, 
+    t.toyType, 
+    t.toyId, 
+    b.timestamp AS boxTimestamp, 
+    b.boxId
+FROM 
+    toys t
+JOIN 
+    boxes b
+ON 
+    t.toyType = b.toyType
+AND 
+    ABS(toUnixTimestamp(t.timestamp) - toUnixTimestamp(b.timestamp)) <= 60
 
