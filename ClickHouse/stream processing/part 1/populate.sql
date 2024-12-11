62b5a936847f7dfc4c593a8eb8697592deaabfd7
paul-brebner <31493691+paul-brebner@users.noreply.github.com>
2024-12-11 13:38:29 +1100

    Update populate.sql

--- ClickHouse/stream processing/part 1/populate.sql
+++ ClickHouse/stream processing/part 1/populate.sql
@@ -1,1 +1,3 @@
+INSERT INTO toys SELECT now() - floor(randUniform(0, 120)), floor(randUniform(0, 100)), number FROM numbers(300)
 
+INSERT INTO boxes SELECT now() - floor(randUniform(0, 120)), floor(randUniform(0, 100)), number FROM numbers(300)
